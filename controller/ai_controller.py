b401061650b69955f17da975720c4f0f5fef9fd3
ksd <aectarine@naver.com>
2025-02-25 16:26:17 +0900

    수정: AI 모듈 CRUD & START/STOP

--- controller/ai_controller.py
+++ controller/ai_controller.py
@@ -1,6 +1,7 @@
 import asyncio
+import uuid
 
-from fastapi import APIRouter, Body, status as res_status, HTTPException
+from fastapi import APIRouter, Body, status as res_status
 
 router = APIRouter(prefix='/ai')
 
@@ -8,98 +9,104 @@ module_list = list()
 
 
 @router.get('', status_code=res_status.HTTP_200_OK)
-async def find_all_ai():
-    return [{'name': ai['name'], 'version': ai['version'], 'status': ai['status']} for ai in module_list]
+async def find_all():
+    return [
+        {'id': module['id'], 'name': module['name'], 'version': module['version'], 'status': module['status']} for module in module_list
+    ]
+
+
+@router.get('/{id}')
+async def find_one(id: str):
+    for module in module_list:
+        if module['id'] == id:
+            return {'id': module['id'], 'name': module['name'], 'version': module['version'], 'status': module['status']}
 
 
 @router.post('')
-async def create_ai(
+async def create(
         name: str = Body(...),
         version: str = Body(...),
         status: str = Body(...)
 ):
-    ai = {
+    id = str(uuid.uuid4())
+    module = {
+        'id': id,
         'name': name,
         'version': version,
-        'status': status
+        'status': status,
     }
-    module_list.append(ai)
+    module_list.append(module)
     return module_list
 
 
-@router.put('', status_code=200)
-async def modify_ai(
+@router.put('/{id}', status_code=200)
+async def modify(
+        id: str,
         name: str = Body(...),
         version: str = Body(...),
         status: str = Body(...)
 ):
     for ai in module_list:
-        if ai['name'] == name:
+        if ai['id'] == id:
+            ai['name'] = name
             ai['version'] = version
             ai['status'] = status
-
     return module_list
 
 
-@router.delete('/{name}')
-async def remove_ai(
-        name: str
+@router.delete('/{id}')
+async def remove(
+        id: str
 ):
     for ai in module_list:
-        if ai['name'] == name:
+        if ai['id'] == id:
             module_list.remove(ai)
     return module_list
 
 
-@router.post('/start')
-async def start_ai_module(
-        name: str = Body(..., embed=True)
-):
-    for ai in module_list:
-        if ai['name'] == name:
-            ai['task'] = asyncio.create_task(ai_module(name))
-            ai['task'].add_done_callback(callback)
-            ai['status'] = 'START'
-    return f'{name} 모듈 구동 시작'
-
-
 async def ai_module(name: str):
-    print(f'{name} 모듈 구동중...')
+    print(f'{name} 모듈 구동 시작...')
     for i in range(0, 100000000000000):
         if i % 10000000 == 0:
-            print('1000000000')
+            print('구동중...')
         await asyncio.sleep(0)
     print(f'{name} 모듈 구동 완료...')
-    # while True:
-    #     print(f'{name} 모듈 구동중...')
-    #     await asyncio.sleep(1)
 
 
 def callback(task):
     try:
+        result = task.result()
         print(f'태스크 {task.get_name()}이(가) 완료됨')
-        print(f'결과: {task.result()}')
+        print(f'결과: {result}')
     except asyncio.CancelledError:
-        print(f'태스크가 취소되었습니다')
+        print(f'태스크 {task.get_name()}이(가) 취소되었습니다')
     except Exception as e:
-        print(f'오류 발생: {e}')
+        print(f'태스크 {task.get_name()}에서 오류 발생: {e}')
 
 
-@router.post('/stop')
-async def stop_ai_module(
-        name: str = Body(..., embed=True)
+@router.get('/start/{id}')
+async def start(
+        id: str
 ):
     for ai in module_list:
-        if ai['name'] == name:
-            ai['task'].cancel()
-            ai['status'] = 'STOP'
-            await asyncio.sleep(0.1)
-    return f'{name} 모듈 구동 시작'
+        if ai['id'] == id and ai['status'] == 'STOP':
+            name = ai['name']
+            task = asyncio.create_task(ai_module(name))
+            task.add_done_callback(callback)
+            ai['task'] = task
+            ai['status'] = 'START'
+            return f'{id} 모듈 구동'
+    return f'{id} 모듈 구동 실패'
 
 
-@router.get('/tasks')
-async def get_all_tasks():
-    current_tasks = asyncio.all_tasks()
-    for task in current_tasks:
-        print(task)
-    return [{'name': task.get_name(), 'done': task.done(), 'cancelled': task.cancelled(), 'state': str(task._state)} for task in current_tasks]
+@router.get('/stop/{id}')
+async def stop(
+        id: str
+):
+    for module in module_list:
+        if module['id'] == id and module['status'] == 'START':
+            module['task'].cancel()
+            module['status'] = 'STOP'
+            await asyncio.sleep(0)
+            return f'{id} 모듈 정지'
+    return f'{id} 모듈 정지 실패'
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from controller.ai_controller import router as ai_router
 from controller.async_controller import router as async_router
 from controller.sync_controller import router as sync_router
 
 app = FastAPI()
-app.include_router(sync_router)
-app.include_router(async_router)
-app.include_router(ai_router)
+
+api_app = APIRouter(prefix='/api')
+
+api_app.include_router(sync_router)
+api_app.include_router(async_router)
+api_app.include_router(ai_router)
+
+app.include_router(api_app)
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app', host='0.0.0.0', port=8001, reload=True)
